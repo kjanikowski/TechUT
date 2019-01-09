9917f3aab8fca323a90d74f045604fe6363e64ff
Krzysztof Janikowski <kjanikowski@sigma.ug.edu.pl>
2019-01-09 16:43:53 +0100

    added skitype

--- zad04/src/main/java/pl/kjanikowski/hibernate/domain/SkiType.java
+++ zad04/src/main/java/pl/kjanikowski/hibernate/domain/SkiType.java
@@ -1,5 +1,36 @@
 package pl.kjanikowski.hibernate.domain;
 
 public class SkiType {
+	
+	private long id;
+	private String Type;
+	
+	public SkiType(String type) {
+		super();
+		Type = type;
+	}
+
+	public SkiType() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return Type;
+	}
+	public void setType(String type) {
+		Type = type;
+	}
+
 
 }
